8e1ba87d99702d1f3ad701b7a4d1bcd1afb5632a
忆霁昕 <yijixin@foxmail.com>
2022-10-11 23:35:22 +0800

    设置cookie 有效期24h

--- src/main/java/com/yijixin/fromzerotoexpert/controller/UserController.java
+++ src/main/java/com/yijixin/fromzerotoexpert/controller/UserController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @Classname UserController
  * @Description TODO
@@ -15,7 +19,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserController {
     @GetMapping("/FromZerotoExpert")
     @ResponseBody
-    public String hello() {
+    public String hello(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("has_accessed")
+                    && cookie.getValue().equals("hafzte")) {
+                return "嗨，欢迎您再次到 from zero to expert.";
+            }
+        }
+        Cookie cookie = new Cookie("has_accessed", "hafzte");
+        cookie.setMaxAge(24 * 60 * 60);
+        response.addCookie(cookie);
         return "嗨，欢迎您来到 from zero to expert.";
     }
 }
